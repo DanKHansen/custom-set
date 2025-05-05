240d9143f056ed54519f83cd99dfa61fb566d1f7
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-05-05 15:44:46 +0200

    in progress

---
+++ src/main/scala/CustomSet.scala
@@ -0,0 +1,12 @@
+object CustomSet:
+   def empty(s: Set[Int]): Boolean = ???
+   def set: Set[Int] = ???
+   def fromList(l: List[Int]): Set[Int] = ???
+   def member(s: Set[Int], i: Int): Boolean = ???
+   def isSubsetOf(s1: Set[Int], s2: Set[Int]) :Boolean = ???
+   def isDisjointFrom(s1: Set[Int], s2: Set[Int]): Boolean = ???
+   def isEqual(s1: Set[Int], s2: Set[Int]): Boolean = ???
+   def insert(s: Set[Int], i: Int): Set[Int] = ???
+   def intersection(s1: Set[Int], s2: Set[Int]): Set[Int] = ???
+   def difference(s1: Set[Int], s2: Set[Int]): Set[Int] = ???
+   def union(s1: Set[Int], s2: Set[Int]): Set[Int] = ???
